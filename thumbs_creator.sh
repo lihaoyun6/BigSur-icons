3973f87efba1759baa073ec710e703cf5ed99259
mac <mac@macdeiMacPro.padavan>
2020-09-10 22:14:41 +0800

    add Wechat,Dash,MATLAB,Eagle
    Former-commit-id: 76635b663c0b7d04081cd1ae42db25bfebc97a53

--- thumbs_creator.sh
+++ thumbs_creator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+old_IFS=$IFS
+IFS=$'\n'
 rm -rf thumbs
 mkdir thumbs
 printf "### 点击小图标可跳转至ICNS原图  /  Click on the thumbnail to access the ICNS file  \n****  \n\n" >> thumbs/thumbs.md
@@ -8,6 +10,8 @@ for i in `ls *.icns`
 do
 	sips -Z 64 -s format jpeg $i --out ../thumbs/${i%.*}.jpg
 	echo '[!['${i%.*}.jpg'](./'${i%.*}.jpg' "'${i%.*}'")](../icons/'${i%.*}.icns')' >> ../thumbs/thumbs.md
+	echo '['${i%.*}'](../icons/'${i%.*}.icns')  ' >> ../thumbs/textlist.md
 done
 cd ..
+IFS=$old_IFS
 echo "Done!"
