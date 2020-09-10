04f28e5ec266303018ddfebaf1fc40dab018e10f
apple <apple@My-iMac.local>
2020-09-10 15:39:40 +0800

    fix thumbs

--- thumbs_creator.sh
+++ thumbs_creator.sh
@@ -5,13 +5,8 @@ mkdir thumbs
 cd icons
 for i in `ls *.icns`
 do
-	sips -s format png $i --out ../thumbs/${i%.*}.png >/dev/null
-done
-cd ../thumbs
-for i in `ls *.png`
-do
-	convert $i -resize 64x64 $i
-	echo '[!['$i'](./'$i' "'${i%.*}'")](../icons/'${i%.*}.icns')' >> thumbs.md
+	sips -Z 64 -s format jpeg $i --out ../thumbs/${i%.*}.jpg
+	echo '[!['${i%.*}.jpg'](./'${i%.*}.jpg' "'${i%.*}'")](../icons/'${i%.*}.icns')' >> ../thumbs/thumbs.md
 done
 cd ..
 echo "Done!"
