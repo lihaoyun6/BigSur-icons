853cff37d751d10f28da89a442c9df85576b30a5
apple <apple@My-iMac.local>
2020-09-11 22:34:52 +0800

    add donation code
    Former-commit-id: ef428ea50656931ae222b95b86d62e6f51687be9

--- thumbs_creator.sh
+++ thumbs_creator.sh
@@ -6,7 +6,7 @@ rm -rf thumbs
 mkdir thumbs
 printf "### 点击小图标可跳转至ICNS原图  /  Click on the thumbnail to access the ICNS file  \n****  \n\n" >> thumbs/thumbs.md
 cd icons
-num=`ls *.icns|wc -l`
+num=`ls *.icns|wc -l|tr -d ' '`
 for i in `ls *.icns`
 do
 	sips -Z 64 -s format jpeg $i --out ../thumbs/${i%.*}.jpg
@@ -17,7 +17,7 @@ do
 done
 cd ..
 sed -i "" '$d' README.md
-echo '[当前共有 '$num' 个图标]/[There are '$num' icons now]' >> README.md
+echo '当前共有 ['$num'] 个图标 / Here are ['$num'] icons now' >> README.md
 IFS=$old_IFS
 git add *
 read -p "请输入commit信息: " commit
